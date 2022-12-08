2e4222a66d94f0a36e47a9ebd7159fa2a4022272
badripaudel77 <badri@eventlogic.se>
2022-12-08 21:40:53 +0545

    Played with the forms well with two way binding, resetting forms, validating and displaying error message in the UI.

--- src/app/search-todo/search-todo.component.ts
+++ src/app/search-todo/search-todo.component.ts
@@ -1,4 +1,5 @@
 import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-search-todo',
@@ -9,30 +10,38 @@ export class SearchTodoComponent implements OnInit {
   @Input() searchTodoTitle:string = '';
   @Input() todos:[]; // Input to this component from the parent component.
   @Output() searchStringEvent = new EventEmitter<string>(); // this component emits the property called searchStringEvent to the parent
+  todoSearchString: string = '';
+  /**
+   * It can access the dom directly via local reference defined in the template any time, without submission etc. in the template as well.
+   * Angular is continuously watching ...
+   */
 
-  @ViewChild('todoSearchInput') todoSearchInput :ElementRef; // It can access the dom directly via local reference defined in the template.
+  @ViewChild('todoSearchInput') todoSearchInput :ElementRef;
+  @ViewChild('myNgTodoForm') myNgTodoForm :NgForm;
 
   name: string = 'Badri';
-  isDisableSubmitButton: boolean = true;
 
   constructor() {
   }
 
   ngOnInit(): void { }
 
-  onInputChange(result: any) {
-    let searchString: string = result.value.todoSearchString.trim().toLowerCase();
-     searchString.length <1 ? this.isDisableSubmitButton = true : this.isDisableSubmitButton = false;
-  }
   /**
    * Emit search query value using searchStringEvent to the parent component.
    * searchStringEvent is decorated by @Output() decorator indicating that it outputs something to parent component.
    * searchStringEvent is used as (searchStringEvent) = "methodname..." in parent component where this component is used as
    * <app-search-todo (searchStringEvent) = "filterTodos($event)"></app-search-todo>
    */
-  onClickSubmit(result:any) {
+  onClickSubmit(result:NgForm) {
     let searchString: string = result.value.todoSearchString.trim().toLowerCase();
     // let searchString: string = this.todoSearchInput.nativeElement.value.trim().toLowerCase(); // same as above
     this.searchStringEvent.emit(searchString);
+    this.myNgTodoForm.reset();
+  }
+
+  setCriticalStatus() {
+    let status: string = 'critical';
+    this.todoSearchInput.nativeElement.value = status;
+    this.todoSearchString = status;
   }
 }
